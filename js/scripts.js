79d018812cfe1b26aa708009eef44bcddf866cbe
JEvans890 <evansjexp@gmail.com>
2023-06-02 16:33:34 -0700

    add a broken name retriever and connect remaining values from html to css

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,39 @@ form.onsubmit = function(event) {
                 }
             }
         }
-
-
+        Phone Apps:{}
+        Video Games:
+        advanced:
+        intermediate:
+        beginner:
+        windows:
+        mac:
+        linux:
+        No Preference:
+        Simple:
+        Concise:
+        Flexible:
+        Popular:
+        Niche:
 
     }
 
+
 const inputField = document.getElementById("inputField");
 const name = inputField.value;
+inputField.value = "";
+let result;
+  if (name === "") {
+    result = "Please enter your name.";
+  } else if (name === "John") {
+    result = "Hello, John!";
+  } else {
+    result = "Hello, " + name + "!";
+  };
+  const resultContainer = document.getElementById("resultContainer");
+  resultContainer.textContent = result;
+  resultContainer.classList.remove("hidden");
+});
 
 event.preventDefault();
 };
