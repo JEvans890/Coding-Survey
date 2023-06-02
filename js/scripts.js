a94ab5434d482fe58c43344f3cdd728f4a6e580b
JEvans890 <evansjexp@gmail.com>
2023-06-02 15:23:06 -0700

    ruin js and be confusing

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 window.onload = function();
 let form = document.querySelector("form");
 form.onsubmit = function(event) {
-    const userinput1 = document.getElementById("userinput1").value;
+    const LanguageSuggestion = {
+        Websites:{
+            beginner: {
+                simple: {
+                    windows:{
+                        popular
+                    }
+                }
+            }
+        }
+
+    }
 
 
     event.preventDefault();
