085ff4d3ea41c7bfaf4ff79f2085f43374d6b312
JEvans890 <evansjexp@gmail.com>
2023-06-02 13:18:39 -0700

    add questions and option selection for them

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 window.onload = function();
 let form = document.querySelector("form");
 form.onsubmit = function(event) {
-    const userinput = document.getElementById("userinput")
+    const userinput1 = document.getElementById("userinput1").value;
+
+
+    event.preventDefault();
 }
